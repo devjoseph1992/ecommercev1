41b3e9f50e0af102a8654deb30b0d49d4d21c3e9
john joseph florina <devjosephflorina@gmail.com>
2020-10-26 13:18:08 +0800

    including node-sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./homepage.style.scss"
+
+const HomePage = () => (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <spa className='subtitle'>SHOP NOW</spa>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Jackets</h1>
+                        <spa className='subtitle'>SHOP NOW</spa>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Sneakers</h1>
+                        <spa className='subtitle'>SHOP NOW</spa>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Womens</h1>
+                        <spa className='subtitle'>SHOP NOW</spa>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Mans</h1>
+                        <spa className='subtitle'>SHOP NOW</spa>
+                    </div>
+                </div>
+            </div>
+        </div>
+)
+
+export default HomePage;
