6b5e17f9c7ec6d8f9fdfaa074f946c4831d9e83c
john joseph florina <devjosephflorina@gmail.com>
2020-11-07 19:53:42 +0800

    complete signing in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInwithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInwithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,8 +16,14 @@ class SignIn extends React.Component{
             password:''
         }
     }
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
+        const { email, password} =this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+        }catch (error) {
+            console.log(error);
+        }
         this.setState({email:'', password:''})
     }
     handleChange = event =>{
