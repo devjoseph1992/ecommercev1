6f78bee5c050c2a0dd746ea961df2db1d042ded7
john joseph florina <devjosephflorina@gmail.com>
2020-11-13 10:59:40 +0800

    redux persist and storage

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -33,7 +33,7 @@ const CheckoutPage = ({cartItems, total}) => (
                 )
         }
         <div className='total'>
-            <sapn>Total: ${total}</sapn>
+            <span>Total: ${total}</span>
         </div>
     </div>
 )
